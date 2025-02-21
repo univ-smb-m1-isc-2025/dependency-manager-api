d8118bb71d0fe1cc479699a7fca424caa44a039e
Oziphos <chevalier.c.contact@gmail.com>
2025-02-21 18:03:39 +0100

    feat: add /api/accounts/signup

--- src/main/java/com/info803/dependency_manager_api/adapters/api/AccountController.java
+++ src/main/java/com/info803/dependency_manager_api/adapters/api/AccountController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -24,16 +26,46 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    /**
+     * Retrieves a list of all accounts.
+     *
+     * @return a list of all Account objects stored in the repository
+     */
+
     @GetMapping
     public List<Account> accountList() {
         logger.info("accountsList");
         return accountService.accountList();
     }
 
+    /**
+     * Retrieves a single account by its id.
+     *
+     * @param id the unique identifier of the account to retrieve
+     * @return an Optional containing the Account object corresponding to the given id, or an empty Optional if no such account exists
+     */
     @GetMapping("/{id}")
     public Optional<Account> account(@PathVariable Long id) {
         logger.info("account");
         return accountService.account(id);
     }
+
+    /**
+     * Creates a new account with the given email and password.
+     *
+     * @param account the Account object containing the email and password
+     * @return a String indicating whether the account was created or already exists
+     */
+    @PostMapping("/signup")
+    public String signUp(@RequestBody Account account) {
+        logger.info("signUp"); 
+        try {
+            accountService.create(account.getMail(), account.getPassword());  
+            return "Account created";
+        } catch (IllegalArgumentException e) {
+            return "Account already exists";
+        }
+    }
+    
     
 }
