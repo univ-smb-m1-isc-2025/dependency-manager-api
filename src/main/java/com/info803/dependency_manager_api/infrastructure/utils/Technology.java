77472fe2d1ba25d8f47d55a307b0e18d8a2f1ba9
Oziphos <chevalier.c.contact@gmail.com>
2025-03-26 14:07:46 +0100

    Update Technology.java

--- src/main/java/com/info803/dependency_manager_api/infrastructure/utils/Technology.java
+++ src/main/java/com/info803/dependency_manager_api/infrastructure/utils/Technology.java
@@ -3,7 +3,9 @@ package com.info803.dependency_manager_api.infrastructure.utils;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Technology {
@@ -17,21 +19,30 @@ public class Technology {
      * @param repoPath Chemin du répertoire du projet
      * @return Les technologies utilisées dans le répertoire sous la forme d'un dictionnaire de paires "technologie" -> "liste des fichiers reconnus"
     */
-    public static List<String> detectTechnologies(String repoPath) {
+    public static Map<String, List<String>> detectTechnologies(String repoPath) {
         File repoDirectory = new File(repoPath);
         if (!repoDirectory.exists() || !repoDirectory.isDirectory()) {
-            return Collections.emptyList();
+            return Collections.emptyMap();
         }
 
         File[] filesInRepo = repoDirectory.listFiles();
-        if (filesInRepo == null) return Collections.emptyList();
+        if (filesInRepo == null) return Collections.emptyMap();
 
-        // Liste des technologies détectées
-        return Arrays.stream(TechnologyType.values())
-                .filter(tech -> Arrays.stream(tech.getFiles())
-                        .anyMatch(fileName -> new File(repoDirectory, fileName).exists()))
-                .map(TechnologyType::getName)
-                .toList();
+        Map<String, List<String>> detectedTechnologies = new HashMap<>();
+
+        for (TechnologyType tech : TechnologyType.values()) {
+            List<String> matchedFiles = Arrays.stream(tech.getFiles())
+                    .map(fileName -> new File(repoDirectory, fileName))
+                    .filter(File::exists)
+                    .map(File::getAbsolutePath)
+                    .collect(Collectors.toList());
+
+            if (!matchedFiles.isEmpty()) {
+                detectedTechnologies.put(tech.getName(), matchedFiles);
+            }
+        }
+
+        return detectedTechnologies;
     }
 
 
