4b1acd613fd009a81af59a95934f7daa7764f6cd
Oziphos <chevalier.c.contact@gmail.com>
2025-03-27 15:14:54 +0100

    Update DepotController.java

--- src/main/java/com/info803/dependency_manager_api/adapters/api/DepotController.java
+++ src/main/java/com/info803/dependency_manager_api/adapters/api/DepotController.java
@@ -18,7 +18,6 @@ import java.util.Optional;
 import java.io.File;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
