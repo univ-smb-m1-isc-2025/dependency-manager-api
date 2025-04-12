bbf11272593fbd874bf7d0b168143872d87f64c2
Oziphos <chevalier.c.contact@gmail.com>
2025-04-12 16:07:23 +0200

    Create EncryptionService.java

---
+++ src/main/java/com/info803/dependency_manager_api/config/EncryptionService.java
@@ -0,0 +1,71 @@
+package com.info803.dependency_manager_api.config;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+@Service
+public class EncryptionService {
+
+    @Value("${security.jwt.secret-key}")
+    private Resource secretKeyFile;
+
+    private SecretKeySpec aesKeySpec;
+
+    @PostConstruct
+    private void init() {
+        try {
+            byte[] keyBytes = Base64.getDecoder().decode(secretKeyFile.getContentAsByteArray());
+            aesKeySpec = new SecretKeySpec(keyBytes, 0, 16, "AES"); // Use first 128 bits (or 256 if supported)
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading encryption key : " + e.getMessage());
+        }
+    }
+
+    public String encrypt(String string) {
+        if (isBase64(string)) {
+            throw new RuntimeException("String already encrypted");
+        }
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, aesKeySpec);
+            byte[] encryptedBytes = cipher.doFinal(string.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting string : " + e.getMessage());
+        }
+    }
+
+    public String decrypt(String string) {
+        if (!isBase64(string)) {
+            throw new RuntimeException("String not encrypted");
+        }
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, aesKeySpec);
+            byte[] decoded = Base64.getDecoder().decode(string);
+            byte[] decryptedBytes = cipher.doFinal(decoded);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error decrypting string : " + e.getMessage());
+        }
+    }
+
+    private boolean isBase64(String token) {
+        try {
+            Base64.getDecoder().decode(token);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
