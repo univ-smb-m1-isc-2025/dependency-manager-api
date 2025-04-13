1ba64a9974f990908644733c3f339d7827915cda
Oziphos <clement.chevalier1@etu.univ-smb.fr>
2025-04-14 01:19:02 +0200

    Update GlobalExceptionHandler.java

--- src/main/java/com/info803/dependency_manager_api/adapters/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/info803/dependency_manager_api/adapters/api/exception/GlobalExceptionHandler.java
@@ -6,16 +6,12 @@ import com.info803.dependency_manager_api.adapters.api.response.ResponseUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
